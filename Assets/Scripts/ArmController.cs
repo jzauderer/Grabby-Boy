718d6f0b5c1b20e01a8be8b72efb80d8cf54382f
Justin Zauderer <jzauds@gmail.com>
2019-01-02 04:35:04 -0500

    Added basic arm model and movement

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+	private Vector3 mousePos;
+
+    void Update()
+    {
+    	mousePos = Input.mousePosition;
+
+    	//Control movement of the arm with an offset
+    	float newX = mousePos.x/Screen.width + .3f;
+    	float newY = mousePos.y/Screen.height + 1;
+    	transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+}
