9c9aa4a598d7b42e116b638ee9b2a1c81c10d931
Justin Zauderer <jzauds@gmail.com>
2019-01-02 21:16:55 -0500

    Added basic throwing

--- Assets/Scripts/Grabbing.cs
+++ Assets/Scripts/Grabbing.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Grabbing : MonoBehaviour
 {
+    //How many frames you have to hold an object in order for it to
+    //receive thrown momentum
+    public int tossTime;
+    public int tossSpeedMult;
+
     private bool grabOn;
     private bool objectGrabbed;
     private Vector3 offset;
+    private Vector3[] positions;
+    private int tossTimeIndex;
 
     void Start()
     {
         grabOn = false;
         objectGrabbed = false;
+        positions = new Vector3[tossTime];
+        tossTimeIndex = 0;
     }
 
     void Update()
@@ -23,20 +32,63 @@ public class Grabbing : MonoBehaviour
     	else
     	{
     		grabOn = false;
+            objectGrabbed = false;
     	}
     }
 
     void OnTriggerStay(Collider other){
     	if(other.gameObject.CompareTag("Grabbable"))
     	{
-    		if(grabOn)
+            //When the player releases the object
+            if(Input.GetMouseButtonUp(0))
+            {
+                //Get rigidbody of the thrown object
+                Rigidbody[] otherRigidBody = other.GetComponentsInChildren<Rigidbody>();
+                //If held long enough, throw it. Otherwise, let it drop
+                if(tossTimeIndex >= tossTime-1)
+                {
+                    Vector3 launchDirection = other.transform.position - positions[0];
+                    Vector3 resultantForce = new Vector3(launchDirection.x/2.0f, launchDirection.y/2.0f, Mathf.Abs(launchDirection.z*3.0f));
+                    otherRigidBody[0].velocity = resultantForce*tossSpeedMult;
+                }
+                else
+                {
+                    otherRigidBody[0].velocity = Vector3.zero;
+                    otherRigidBody[0].angularVelocity = Vector3.zero;
+                }
+                //Reset array of positions for grabbed object
+                positions = new Vector3[tossTime];
+                tossTimeIndex = 0;
+            }
+            //If left click is held
+    		else if(grabOn)
     		{
+                //Set offset distance upon first grabbing
     			if(!objectGrabbed)
     			{
     				objectGrabbed = true;
     				offset = (other.transform.position - transform.position)/3;
     			}
     			other.transform.position = transform.position + offset;
+
+                /*
+                positions stores the positions of the object over the last
+                x frames, x being tossTime. If positions is not full, we have 
+                not held the object long enough to throw it with force
+                */ 
+                if(tossTimeIndex < tossTime)
+                {
+                    positions[tossTimeIndex] = other.transform.position;
+                    tossTimeIndex++;
+                }
+                else
+                {
+                    for(int i = 1; i < tossTimeIndex; i++)
+                    {
+                        positions[i-1] = positions[i];
+                    }
+                    positions[tossTime-1] = other.transform.position;
+                }
     		}
     	}
     }
